7e27407305fa5738eb7b1200b651eae91d6cd86c
Sergio Leech <0b11100001100@gmail.com>
2021-01-14 19:40:23 +0200

    Change NewsViewModel

--- app/src/main/java/com/example/usatechnologynews/view_model/NewsViewModel.kt
+++ app/src/main/java/com/example/usatechnologynews/view_model/NewsViewModel.kt
@@ -1,10 +1,7 @@
 package com.example.usatechnologynews.view_model
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.usatechnologynews.model.Article
 import com.example.usatechnologynews.model.ListType
 import com.example.usatechnologynews.repositories.NewsRepository
@@ -12,22 +9,13 @@ import kotlinx.coroutines.launch
 
 class NewsViewModel @ViewModelInject constructor(private val repository: NewsRepository):ViewModel() {
 
-   private val _newsList= MutableLiveData<List<Article>>()
-    val newsList:LiveData<List<Article>>
-    get() = _newsList
+    val newsList= liveData {
+        emit(repository.getNewsList())
+  }
 
     private val _newsListType: MutableLiveData<ListType> = MutableLiveData()
     val newsListType: LiveData<ListType>
         get() = _newsListType
-
-    init {
-        getNewsList()
-    }
-    private fun getNewsList(){
-        viewModelScope.launch {
-            _newsList.value= repository.getNewsList()
-        }
-    }
 
     fun getNewsById(id:String): Article? = newsList.value?.find { news -> news.url == id }
 
